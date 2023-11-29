58704d8c4f7ded03dff033ae5cc0686680c39721
hongyi-laptop-wsl <henryliuw@gmail.com>
2023-11-29 14:32:49 -0500

    basic animation

--- src/user_interface.ml
+++ src/user_interface.ml
@@ -5,46 +5,74 @@ open Math
 open Rasterizer
 open Ascii_printer
 
-(* let () = *)
- let rec looping () =
+let display_static_image display_type alpha beta img_w : unit =
+  match display_type with
+  | "planar" ->
+      let img =
+        getImage ~img_w ~view_size:4 ~plane_bd:4 ~half_edge_length:2
+          ~line_w:0.25 ~grid_size:2 Planar ~alpha:(Degree.of_float alpha)
+          ~beta:(Degree.of_float beta)
+          ~center:(Vec3.of_list [ 0.; 0.; 1. ])
+      in
+      let () = print_ascii_image img img_w in
+      ()
+  | "sphere" ->
+      let img =
+        getImage ~img_w ~view_size:4 ~plane_bd:4 ~half_edge_length:2
+          ~line_w:0.25 ~grid_size:2 Sphere ~alpha:(Degree.of_float alpha)
+          ~beta:(Degree.of_float beta)
+          ~center:(Vec3.of_list [ 0.; 0.; 1. ])
+      in
+      let () = print_ascii_image img img_w in
+      ()
+  | "orthogonal" ->
+      let img =
+        getImage ~img_w ~view_size:4 ~plane_bd:4 ~half_edge_length:2
+          ~line_w:0.25 ~grid_size:2 Orthogonal ~alpha:(Degree.of_float alpha)
+          ~beta:(Degree.of_float beta)
+          ~center:(Vec3.of_list [ 0.; 0.; 1. ])
+      in
+      let () = print_ascii_image img img_w in
+      ()
+  | _ ->
+      let () =
+        print_endline
+          "display type command line argument should be one of 'planar', \
+           'sphere', or 'orthogonal' "
+      in
+      ()
+
+let animate_beta ~low ~high frame_rate display_type alpha img_w =
+  let sleep_time = 1. /. float_of_int frame_rate in
+  let betas =
+    List.range 0 frame_rate
+    |> List.map ~f:(fun x -> float_of_int x *. (high -. low) *. sleep_time)
+  and redraw_and_sleep beta =
+    display_static_image display_type alpha beta img_w; Caml_unix.sleepf sleep_time
+  in
+  List.iter betas ~f:(fun beta ->
+      redraw_and_sleep beta)
+
+let rec looping () =
   Out_channel.output_string stdout "enter something: \n";
   Out_channel.flush stdout;
   match In_channel.(input_line stdin) with
   | None -> looping ()
-  | Some "exit" -> ();
-  | Some s ->
-    Out_channel.output_string stdout 
-         ("You just input: " ^ s ^"\n");
-    Out_channel.flush stdout;
-    looping () (* do something here, probably update internal values and redraw the ascii images *)
-  (* in looping () *)
+  | Some "exit" -> ()
+  | Some s -> (
+      Out_channel.output_string stdout ("You just input: " ^ s ^ "\n");
+      Out_channel.flush stdout;
+      match float_of_string_opt s with
+      | None ->
+          Out_channel.output_string stdout
+            "the value cannot be convert to a float\n";
+          looping ()
+      | Some f ->
+          animate_beta ~low:0. ~high:f 30 "orthogonal" 0. 50;
+          looping ())
 
-let display_static_image display_type alpha beta image_w : unit =
-  match display_type with
-  | "planar" -> 
-    let img =
-      getImage ~img_w:image_w ~view_size:4 ~plane_bd:4 ~half_edge_length:2 ~line_w:0.25
-          ~grid_size:2 Planar ~alpha:(Degree.of_float alpha) ~beta:(Degree.of_float beta)
-          ~center:(Vec3.of_list [ 0.; 0.; 1. ]) in
-    let () = print_ascii_image img image_w in ()
-  | "sphere" -> 
-    let img =
-      getImage ~img_w:image_w ~view_size:4 ~plane_bd:4 ~half_edge_length:2 ~line_w:0.25
-          ~grid_size:2 Sphere ~alpha:(Degree.of_float alpha) ~beta:(Degree.of_float beta)
-          ~center:(Vec3.of_list [ 0.; 0.; 1. ]) in
-    let () = print_ascii_image img image_w in ()  
-  | "orthogonal" -> 
-    let img =
-      getImage ~img_w:image_w ~view_size:4 ~plane_bd:4 ~half_edge_length:2 ~line_w:0.25
-          ~grid_size:2 Orthogonal ~alpha:(Degree.of_float alpha) ~beta:(Degree.of_float beta)
-          ~center:(Vec3.of_list [ 0.; 0.; 1. ]) in
-    let () = print_ascii_image img image_w in ()  
-  | _ ->
-    let () = print_endline "display type command line argument should be one of 'planar', 'sphere', or 'orthogonal' "
-    in ()
-
-
-
+(* do something here, probably update internal values and redraw the ascii images *)
+(* in looping () *)
 
 (* command line argument parsing *)
 let command =
@@ -54,22 +82,34 @@ let command =
        sample from n-gram distribution to make a sentence"
     (* command line arguments are below *)
     (let%map_open.Command display_type =
-        flag "--display-type" (optional_with_default ("") string) ~doc:"The type of display: sphere, planar, or orthogonal"
-      and alpha_parameter =
-        flag "--alpha" (optional_with_default (-1.) float) ~doc:"alpha parameter in moebius transformation"
-      and beta_parameter =
-        flag "--beta" (optional_with_default (-1.) float) ~doc:"beta parameter in moebius transformation"
-      and image_w_parameter =
-        flag "--image-width" (optional_with_default (-1) int) ~doc:"image-width parameter in moebius transformation"
-     (* and remaining_args = anon (sequence ("remaining" %: string)) *)
-    in
+       flag "--display-type"
+         (optional_with_default "" string)
+         ~doc:"The type of display: sphere, planar, or orthogonal"
+     and alpha_parameter =
+       flag "--alpha"
+         (optional_with_default (-1.) float)
+         ~doc:"alpha parameter in moebius transformation"
+     and beta_parameter =
+       flag "--beta"
+         (optional_with_default (-1.) float)
+         ~doc:"beta parameter in moebius transformation"
+     and img_w_parameter =
+       flag "--image-width"
+         (optional_with_default (-1) int)
+         ~doc:"image-width parameter in moebius transformation"
+       (* and remaining_args = anon (sequence ("remaining" %: string)) *)
+     in
      fun () ->
        (* calling the main logic of the program *)
-       if String.(display_type = "") && Float.(alpha_parameter = -1.) && Float.(beta_parameter = -1.) && image_w_parameter = -1 then
-        looping ()
+       if
+         String.(display_type = "")
+         && Float.(alpha_parameter = -1.)
+         && Float.(beta_parameter = -1.)
+         && img_w_parameter = -1
+       then looping ()
        else
-       display_static_image display_type alpha_parameter beta_parameter image_w_parameter
-    )
+         display_static_image display_type alpha_parameter beta_parameter
+           img_w_parameter)
 
 (* the actual main method of the program where the program begins running *)
 let () =
